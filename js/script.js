f68727d1d5aebf539efae513181df0e53adeaf72
Juan Jimenez <jimenez.f.juan.m@gmail.com>
2017-10-29 23:35:21 -0400

    Project updated

--- js/script.js
+++ js/script.js
@@ -14,12 +14,20 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 var dc = {};
 
 var homeHtmlUrl = "snippets/home-snippet.html";
-var allCategoriesUrl = 
-  "https://davids-restaurant.herokuapp.com/categories.json";
+
+/*var allCategoriesUrl = 
+  "https://davids-restaurant.herokuapp.com/categories.json";*/
+
+var allCategoriesUrl = "http://localhost:3000/categories.json";
+
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
-var menuItemsUrl = 
-  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+
+/*var menuItemsUrl = 
+  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";*/
+
+var menuItemsUrl = "http://localhost:3000/menu_items.json?category=";
+
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
