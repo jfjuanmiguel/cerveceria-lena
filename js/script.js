e59aa1f99609dfbab0b5d0f08ef7515be3f5c331
Juan Jimenez <jimenez.f.juan.m@gmail.com>
2017-11-20 02:08:23 -0400

    specials link updated

--- js/script.js
+++ js/script.js
@@ -58,16 +58,17 @@ var switchMenuToActive = function () {
   // Remove 'active' from home button
   var classes = document.querySelector("#navHomeButton").className;
   classes = classes.replace(new RegExp("active", "g"), "");
-  document.querySelector("#navHomeButton").className = classes;
-  document.querySelector("#navMenuButton").className = classes;
-  document.querySelector("#navCategoryButton").className = classes;
+  document.querySelector("#navHomeButton").className = classes;  
 
   // Add 'active' to menu button if not already there
   classes = document.querySelector("#navMenuButton").className;  
   if (classes.indexOf("active") == -1) {    
     classes += " active";
     document.querySelector("#navMenuButton").className = classes;
-  } else {
+  } 
+
+  classes = document.querySelector("#navCategoryButton").className;  
+  if (classes.indexOf("active") == -1) {
     classes += " active";
     document.querySelector("#navCategoryButton").className = classes;
   }
